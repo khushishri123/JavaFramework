a119d3c5e74fb305a8545b67d7d94d8882f7b08d
khushishri123 <107344275+khushishri123@users.noreply.github.com>
2022-06-12 17:24:32 +0530

    Delete hr directory
    Delete hr dir.

--- hr/netserver/src/main/java/com/thinking/machines/network/server/RequestHandlerInterface.java
+++
@@ -1,6 +0,0 @@
-package com.thinking.machines.network.server;
-import com.thinking.machines.network.common.*;
-public interface RequestHandlerInterface
-{
-public Response process(Request request);
-}
--- hr/tmp/eg1.java
+++
@@ -1,6 +0,0 @@
-class StudentDTO
-{
-private int rollNumber;
-private String name;
-private String course;
-}
